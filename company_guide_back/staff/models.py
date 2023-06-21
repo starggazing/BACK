0449b171eaee2b4df14811383fb1a77ba55b5e83
starggazing <strmitwhatilike@gmail.com>
2023-06-21 17:02:06 +0300

    create events

--- company_guide_back/staff/models.py
+++ company_guide_back/staff/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Events(models.Model):
+    name = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+    responsibilities = models.CharField(max_length=1000)
